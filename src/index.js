7348aff2ab7a5d9b57884b50e18d8be1fc233184
unknown <fqtsz2014@163.com>
2019-11-13 14:21:23 +0800

    webpack配置

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import '../css/style.css';
 import '../css/reset.css';
 import '../css/mix.less';
+
+// 还需要在主要的js文件里写入下面这段代码
+let person = {
+  name: 'fqt',
+  age: 12
+};
+Object.defineProperty(person, 'sex', {
+  writable: false,
+  value: '女'
+});
+
+console.log(person);
+
+// if (module.hot) {
+//   // 实现热更新
+//   module.hot.accept();
+// }
+
+const PI = 3.1415926;
+
+console.log(PI);
+
+console.log($);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');  // 引入路径模块包
 const HtmlWepackPlugin = require('html-webpack-plugin');  // 配置 html模板
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // 拆分css样式插件
-
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');  // 清除打包之前的文件
+const Webpack = require('webpack');  // 引入 webpack 包
 // 正常引入de less
 const styleLess = new ExtractTextWebpackPlugin('css/style.css');
 const resetCss = new ExtractTextWebpackPlugin('css/reset.css');
@@ -21,7 +22,8 @@ module.exports = {
         //use: ['style-loader', 'css-loader']  // 从右向左解析
         use: styleLess.extract({
           // 将 css 用link的方式引入就不需要用 style-loader了
-          use: 'css-loader'
+          use: 'css-loader',
+          publicPath: '../'
         })
       },
       {  // 解析less
@@ -29,6 +31,32 @@ module.exports = {
         use: resetCss.extract({
           use: ['css-loader', 'less-loader']
         })
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,  // 小于8k的图片自动转成base64格式，并且不会存在实体图片
+              outputPath: 'images/' // 图片打包后存放的目录
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(htm| html)$/,
+        use: 'html-withimg-loader'
+      },
+      {
+        test: /\.(eot|ttf|woff|svg)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        include: /src/,  // 只转化src目录下的js
+        exclude: /node_modules/  // 排除掉 node_modules, 优化打包速度
       }
     ]
   },                      
@@ -43,8 +71,39 @@ module.exports = {
     //拆分后会把 css 文件放到 dist 目录下的 css/style.css
     styleLess,
     resetCss,
-    mixLess
+    mixLess,
+    new CleanWebpackPlugin(),
+    new Webpack.HotModuleReplacementPlugin()  // 热更新，不是刷新
   ],       
-  devServer: {},     // 开发服务器配置
-  mode: 'development' // 模式配置
+  devServer: {          // 开发服务器配置
+    contentBase: './dist',
+    host: 'localhost',  // 默认是 localhost
+    port: 3000,         // 端口
+    open: false,        // 默认不自动打开浏览器 
+    hot: true           // 开启热更新
+  },    
+  mode: 'development', // 模式配置
+  resolve: {
+    alias: {  //别名
+
+    },
+    extensions: ['.js', '.json', '.css']
+  },
+  optimization: {  // 提取公共代码
+    splitChunks: {
+      cacheGroups: {
+        vendor: { // 抽离第三方插件
+           test: /node_modules/, //指定是 node_modules 下的第三方包 
+           chunks: 'initial',
+           name: 'vendor',  // 打包后的文件名， 任意命名
+           priority: 10 // 设置优先级，防止和自定义的公共代码提取时被覆盖， 不进行打包
+        },
+        utils: {
+          chunks: 'initial',
+          name: 'utils', // 任意命名
+          minSize: 0     // 只要超出0字节就生成新包
+        }
+      }
+    }
+  }
 }
