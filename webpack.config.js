b286520be2ad73f9fe99cd8ba9078fdc0b30baee
unknown <fqtsz2014@163.com>
2019-11-13 11:34:25 +0800

    配置 css 和

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');  // 引入路径模块包
 const HtmlWepackPlugin = require('html-webpack-plugin');  // 配置 html模板
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // 拆分css样式插件
+
+// 正常引入de less
+const styleLess = new ExtractTextWebpackPlugin('css/style.css');
+const resetCss = new ExtractTextWebpackPlugin('css/reset.css');
+const mixLess = new ExtractTextWebpackPlugin('css/mix.css');
 
 module.exports = {
   entry: './src/index.js',         // 入口文件
@@ -8,7 +14,24 @@ module.exports = {
     filename: 'bundle.[hash:4].js',         // 打包后的文件名称
     path: path.resolve('dist')     // 打包后的目录，必须是绝对路径         
   },       
-  module: {},                      // 处理对应的模块
+  module: {                        // 处理对应的模块 
+    rules: [
+      {
+        test: /\.css$/,            //解析css
+        //use: ['style-loader', 'css-loader']  // 从右向左解析
+        use: styleLess.extract({
+          // 将 css 用link的方式引入就不需要用 style-loader了
+          use: 'css-loader'
+        })
+      },
+      {  // 解析less
+        test: /\.less$/,
+        use: resetCss.extract({
+          use: ['css-loader', 'less-loader']
+        })
+      }
+    ]
+  },                      
   plugins: [                       // 开发服务器配置 
     // 通过 new 一下这个类来使用插件
     new HtmlWepackPlugin({
@@ -16,7 +39,11 @@ module.exports = {
       // 在 src目录下创建一个 index.html 页面当做模板来用
       template: './src/index.html',
       hash: true  // 会在打包好的 bundle.js 后面加上 hash 串
-    })
+    }),
+    //拆分后会把 css 文件放到 dist 目录下的 css/style.css
+    styleLess,
+    resetCss,
+    mixLess
   ],       
   devServer: {},     // 开发服务器配置
   mode: 'development' // 模式配置
